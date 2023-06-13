91c239c023eda2ed491798e72f10e68f55ad450f
tristanvrnn <134289144+tristanvrnn@users.noreply.github.com>
2023-06-13 17:58:12 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Dictionary<char, List<string>> dictionary = new Dictionary<char, List<string>>();
+
+        // Add words to the dictionary
+        dictionary.Add('a', new List<string> { "Arbre", "Avion", "Animal" });
+        dictionary.Add('b', new List<string> { "Bateau", "Bougie", "Banane" });
+        dictionary.Add('c', new List<string> { "Chat", "Chien", "Carotte" });
+
+        dictionary.Remove('c');
+
+         foreach(KeyValuePair<char, List<string>> entry in dictionary)
+        {
+            Console.WriteLine(entry.Key + " ==> " + String.Join(" ; ", entry.Value));
+        }
+    }   
+
+}
